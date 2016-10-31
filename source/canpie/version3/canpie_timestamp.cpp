56a8cdd1ec63285d63cad33caea3b4684d4573e7
Uwe Koppe <koppe@microcontrol.net>
2016-10-31 14:09:44 +0100

    Add all required operators

--- source/canpie/version3/canpie_timestamp.cpp
+++ source/canpie/version3/canpie_timestamp.cpp
@@ -1,6 +1,6 @@
 //============================================================================//
 // File:          canpie_timestamp.cpp                                        //
-// Description:   CANpie classes - timestamp                                  //
+// Description:   CANpie classes - time-stamp                                 //
 //                                                                            //
 // Copyright (C) MicroControl GmbH & Co. KG                                   //
 // 53844 Troisdorf - Germany                                                  //
@@ -32,7 +32,6 @@
 ** Include files                                                              **
 **                                                                            **
 \*----------------------------------------------------------------------------*/
-
 #include "canpie_timestamp.hpp"
 
 
@@ -40,18 +39,18 @@
 
 //----------------------------------------------------------------------------//
 // CpTimeStamp()                                                              //
-// timestamp constructor                                                      //
+// time-stamp constructor                                                     //
 //----------------------------------------------------------------------------//
-CpTimeStamp::CpTimeStamp()
+CpTimeStamp::CpTimeStamp(uint32_t ulSecondsV, uint32_t ulNanoSecondsV)
 {
-   ulSecondsP     = 0;
-   ulNanoSecondsP = 0;
+   this->setSeconds(ulSecondsV);
+   this->setNanoSeconds(ulNanoSecondsV);
 }
 
 
 //----------------------------------------------------------------------------//
 // clear()                                                                    //
-// clear timestamp value                                                      //
+// clear time-stamp value                                                     //
 //----------------------------------------------------------------------------//
 void CpTimeStamp::clear(void)
 {
@@ -62,21 +61,275 @@ void CpTimeStamp::clear(void)
 
 //----------------------------------------------------------------------------//
 // fromMicroSeconds()                                                         //
-// convert micro-seconds value to timestamp value                             //
+// convert micro-seconds value to time-stamp value                            //
 //----------------------------------------------------------------------------//
-void CpTimeStamp::fromMicroSeconds(uint64_t uqMicroSecondsV)
+void CpTimeStamp::fromMicroSeconds(uint32_t ulMicroSecondsV)
 {
-
+   //----------------------------------------------------------------
+   // calculate the seconds
+   //
+   ulSecondsP     = ulMicroSecondsV / 1000000UL;
+   
+   //----------------------------------------------------------------
+   // substract the seconds from parameter
+   //
+   ulMicroSecondsV = ulMicroSecondsV - (ulSecondsP * 1000000UL);
+   
+   //----------------------------------------------------------------
+   // convert the remaining part to nano-seconds
+   //
+   ulNanoSecondsP = ulMicroSecondsV * 1000UL;
 }
+
 
 //----------------------------------------------------------------------------//
 // fromMilliSeconds()                                                         //
-// convert milli-seconds value to timestamp value                             //
+// convert milli-seconds value to time-stamp value                            //
 //----------------------------------------------------------------------------//
-void CpTimeStamp::fromMilliSeconds(uint64_t uqMilliSecondsV)
+void CpTimeStamp::fromMilliSeconds(uint32_t ulMilliSecondsV)
 {
+   //----------------------------------------------------------------
+   // calculate the seconds
+   //
+   ulSecondsP     = ulMilliSecondsV / 1000UL;
+   
+   //----------------------------------------------------------------
+   // substract the seconds from parameter
+   //
+   ulMilliSecondsV = ulMilliSecondsV - (ulSecondsP * 1000UL);
+   
+   //----------------------------------------------------------------
+   // convert the remaining part to nano-seconds
+   //
+   ulNanoSecondsP = ulMilliSecondsV * 1000000UL;
 
 }
+
+
+//----------------------------------------------------------------------------//
+// isValid()                                                                  //
+// test valid value range for data fields                                     //
+//----------------------------------------------------------------------------//
+bool CpTimeStamp::isValid(void)
+{
+   bool  btResultT = false;
+
+   if( (ulSecondsP <= TIME_STAMP_SECS_LIMIT) &&
+       (ulNanoSecondsP <= TIME_STAMP_NSEC_LIMIT) )
+   {
+      btResultT = true;
+   }
+   
+   return(btResultT);
+}
+
+
+//----------------------------------------------------------------------------//
+// operator -                                                                 //
+// substract two time-stamp values                                            //
+//----------------------------------------------------------------------------//
+CpTimeStamp CpTimeStamp::operator-(const CpTimeStamp & clTimestampR) const
+{
+   CpTimeStamp clTempT(*this);
+
+   clTempT -= clTimestampR;   // use operator -=
+   return(clTempT);
+}
+
+
+//----------------------------------------------------------------------------//
+// operator !=                                                                //
+// compare two time-stamp values                                              //
+//----------------------------------------------------------------------------//
+bool CpTimeStamp::operator!=(const CpTimeStamp & clTimestampR) const
+{
+   return(!(*this == clTimestampR));
+}
+
+
+//----------------------------------------------------------------------------//
+// operator +                                                                 //
+// add two time-stamp values                                                  //
+//----------------------------------------------------------------------------//
+CpTimeStamp  CpTimeStamp::operator+(const CpTimeStamp & clTimestampR) const
+{
+   CpTimeStamp clTempT(*this);
+
+   clTempT += clTimestampR;   // use operator +=
+   return(clTempT);
+}
+
+
+//----------------------------------------------------------------------------//
+// operator +=                                                                //
+// add two time-stamp values                                                  //
+//----------------------------------------------------------------------------//
+CpTimeStamp & CpTimeStamp::operator+=(const CpTimeStamp & clTimestampR) 
+{
+   uint32_t    ulNanoSecsT = 0;
+   uint64_t    uqSecondsT = 0;
+
+   //----------------------------------------------------------------
+   // Add nano-second values, this may lead to an overflow, i.e.
+   // more than 999.999.999 nano-seconds
+   //
+   ulNanoSecsT = this->nanoSeconds() + clTimestampR.nanoSeconds();
+   if(ulNanoSecsT > TIME_STAMP_NSEC_LIMIT)
+   {
+      uqSecondsT  = 1;
+      ulNanoSecsT = ulNanoSecsT - 1000000000UL;
+   }
+
+   //----------------------------------------------------------------
+   // calculate seconds value
+   //
+   uqSecondsT = uqSecondsT + this->seconds();
+   uqSecondsT = uqSecondsT + clTimestampR.seconds();
+
+   //----------------------------------------------------------------
+   // check for overflow
+   //
+   if(uqSecondsT > TIME_STAMP_SECS_LIMIT)
+   {
+      //---------------------------------------------------
+      // set invalid value
+      //
+      this->ulSecondsP     = TIME_STAMP_INVALID_VALUE; 
+      this->ulNanoSecondsP = TIME_STAMP_INVALID_VALUE;
+   }
+   else
+   {
+      this->setSeconds((uint32_t) uqSecondsT);
+      this->setNanoSeconds(ulNanoSecsT);
+   }
+   
+   return(*this);
+}
+
+
+//----------------------------------------------------------------------------//
+// operator <                                                                 //
+// compare two time-stamp values                                              //
+//----------------------------------------------------------------------------//
+bool CpTimeStamp::operator<(const CpTimeStamp & clTimestampR) const
+{
+   bool btResultT = false;
+
+   //----------------------------------------------------------------
+   // when seconds part is smaller, then result is true
+   //
+   if(this->seconds() < clTimestampR.seconds())
+   {
+      btResultT = true;
+   }
+
+   //----------------------------------------------------------------
+   // when seconds part is equal, check the nano-seconds
+   //
+   if(this->seconds() == clTimestampR.seconds())
+   {
+      if(this->nanoSeconds() < clTimestampR.nanoSeconds())
+      {
+         btResultT = true;
+      }
+   }
+      
+   return(btResultT);
+}
+
+
+//----------------------------------------------------------------------------//
+// operator <=                                                                //
+// compare two time-stamp values                                              //
+//----------------------------------------------------------------------------//
+bool CpTimeStamp::operator<=(const CpTimeStamp & clTimestampR) const
+{
+   return((*this == clTimestampR) || (*this < clTimestampR));
+}
+
+
+//----------------------------------------------------------------------------//
+// operator -=                                                                //
+// substract two time-stamp values                                            //
+//----------------------------------------------------------------------------//
+CpTimeStamp & CpTimeStamp::operator-=(const CpTimeStamp & clTimeStampR)
+{
+   uint32_t    ulNanoSecsT = 0;
+   uint32_t    ulSecondsT  = 0;
+   
+   //----------------------------------------------------------------
+   // test is substraction may cause an underflow
+   //
+   if(*this < clTimeStampR)
+   {
+      //---------------------------------------------------
+      // set invalid value
+      //
+      this->ulSecondsP     = TIME_STAMP_INVALID_VALUE; 
+      this->ulNanoSecondsP = TIME_STAMP_INVALID_VALUE;
+   }
+   else
+   {
+      if(this->ulNanoSecondsP < clTimeStampR.ulNanoSecondsP)
+      {
+         ulNanoSecsT = this->ulNanoSecondsP   + 1000000000UL;
+         ulNanoSecsT = ulNanoSecsT - clTimeStampR.ulNanoSecondsP;
+         ulSecondsT  = (this->ulSecondsP)     - (clTimeStampR.ulSecondsP);
+         ulSecondsT  = ulSecondsT - 1;
+      }
+      else
+      {
+         ulNanoSecsT = (this->ulNanoSecondsP) - (clTimeStampR.ulNanoSecondsP);
+         ulSecondsT  = (this->ulSecondsP)     - (clTimeStampR.ulSecondsP);
+      }
+      
+      //---------------------------------------------------
+      // set value
+      //
+      this->ulSecondsP     = ulSecondsT; 
+      this->ulNanoSecondsP = ulNanoSecsT;
+   }
+   
+   return(*this);
+}
+
+//----------------------------------------------------------------------------//
+// operator ==                                                                //
+// compare two time-stamp values                                               //
+//----------------------------------------------------------------------------//
+bool CpTimeStamp::operator==(const CpTimeStamp & clTimeStampR) const
+{
+   bool btResultT = false;
+   if(this->nanoSeconds() == clTimeStampR.nanoSeconds())
+   {
+      if(this->seconds() == clTimeStampR.seconds())
+      {
+         btResultT = true;
+      }
+   }
+   return(btResultT);
+}
+
+
+//----------------------------------------------------------------------------//
+// operator >                                                                 //
+// compare two time-stamp values                                               //
+//----------------------------------------------------------------------------//
+bool CpTimeStamp::operator>(const CpTimeStamp & clTimestampR) const
+{
+   return(!(*this < clTimestampR) && !(*this == clTimestampR));
+}
+
+
+//----------------------------------------------------------------------------//
+// operator >=                                                                 //
+// compare two time-stamp values                                               //
+//----------------------------------------------------------------------------//
+bool CpTimeStamp::operator>=(const CpTimeStamp & clTimestampR) const
+{
+   return((*this == clTimestampR) || !(*this < clTimestampR));
+}
+
 
 //----------------------------------------------------------------------------//
 // setNanoSeconds()                                                           //
@@ -84,102 +337,29 @@ void CpTimeStamp::fromMilliSeconds(uint64_t uqMilliSecondsV)
 //----------------------------------------------------------------------------//
 void CpTimeStamp::setNanoSeconds(uint32_t ulNanoSecondsV)
 {
-   if(ulNanoSecondsV < 1000000000UL)
+   if(ulNanoSecondsV <= TIME_STAMP_NSEC_LIMIT)
    {
       ulNanoSecondsP = ulNanoSecondsV;
    }
    else
    {
-      ulNanoSecondsP = 0;
+      ulNanoSecondsP = TIME_STAMP_NSEC_LIMIT;
    }
 }
 
 
 //----------------------------------------------------------------------------//
-// operator ==                                                                //
-// compare two timestamp values                                               //
+// setSeconds()                                                               //
+// set seconds value                                                          //
 //----------------------------------------------------------------------------//
-bool CpTimeStamp::operator==(const CpTimeStamp & clTimestampR)
+void CpTimeStamp::setSeconds(const uint32_t ulSecondsV)
 {
-   bool btResultT = false;
-
-   if(this->nanoSeconds() == clTimestampR.nanoSeconds())
+   if(ulSecondsV <= TIME_STAMP_SECS_LIMIT)
    {
-      if(this->seconds() == clTimestampR.seconds())
-      {
-         btResultT = true;
-      }
+      ulSecondsP = ulSecondsV;
    }
-
-   return(btResultT);
-}
-
-
-//----------------------------------------------------------------------------//
-// operator ==                                                                //
-// compare two timestamp values                                               //
-//----------------------------------------------------------------------------//
-bool CpTimeStamp::operator!=(const CpTimeStamp & clTimestampR)
-{
-   return(!(*this == clTimestampR));
-}
-
-
-//----------------------------------------------------------------------------//
-// operator <                                                                 //
-// compare two timestamp values                                               //
-//----------------------------------------------------------------------------//
-bool CpTimeStamp::operator<(const CpTimeStamp & clTimestampR)
-{
-   bool btResultT = false;
-
-
-   return(btResultT);
-
-}
-
-
-//----------------------------------------------------------------------------//
-// operator <                                                                 //
-// compare two timestamp values                                               //
-//----------------------------------------------------------------------------//
-bool CpTimeStamp::operator>(const CpTimeStamp & clTimestampR)
-{
-   bool btResultT = false;
-
-
-   return(btResultT);
-
-}
-
-
-//----------------------------------------------------------------------------//
-// operator +                                                                 //
-// add two timestamp values                                                   //
-//----------------------------------------------------------------------------//
-CpTimeStamp CpTimeStamp::operator+(const CpTimeStamp & clTimestampR)
-{
-   CpTimeStamp clResultT;
-   uint32_t    ulNanoSecsT = 0;
-   uint32_t    ulSecondsT = 0;
-
-   //----------------------------------------------------------------
-   // Add nano-second values, this may lead to an overflow, i.e.
-   // more than 999.999.999 nano-seconds
-   //
-   ulNanoSecsT = this->nanoSeconds() + clTimestampR.nanoSeconds();
-   if(ulNanoSecsT > 999999999)
+   else
    {
-      ulSecondsT = 1;
-      clResultT.setNanoSeconds(ulNanoSecsT - 1000000000UL);
-   }
-
-   //----------------------------------------------------------------
-   // calculate seconds value
-   //
-   ulSecondsT = ulSecondsT + this->seconds();
-   ulSecondsT = ulSecondsT + clTimestampR.seconds();
-
-   return(clResultT);
-
+      ulSecondsP = TIME_STAMP_SECS_LIMIT;
+   }   
 }
