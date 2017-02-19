5e6b2e2a81ee5b5e637aff06279ded6f4b4a03b7
U. Koppe <koppe@microcontrol.net>
2017-02-19 18:54:00 +0100

    Re-design the implementation, no inheritance from CANpie classes any more.

--- source/qcan/qcan_frame_api.cpp
+++ source/qcan/qcan_frame_api.cpp
@@ -1,34 +1,31 @@
 //============================================================================//
 // File:          qcan_frame_api.cpp                                          //
-// Description:   QCAN Server - Network implementation                        //
-// Author:        Uwe Koppe                                                   //
-// e-mail:        koppe@microcontrol.net                                      //
+// Description:   QCan classes - CAN API frame                                //
 //                                                                            //
 // Copyright (C) MicroControl GmbH & Co. KG                                   //
-// Junkersring 23                                                             //
-// 53844 Troisdorf                                                            //
-// Germany                                                                    //
-// Tel: +49-2241-25659-0                                                      //
-// Fax: +49-2241-25659-11                                                     //
+// 53844 Troisdorf - Germany                                                  //
+// www.microcontrol.net                                                       //
 //                                                                            //
-// The copyright to the computer program(s) herein is the property of         //
-// MicroControl GmbH & Co. KG, Germany. The program(s) may be used            //
-// and/or copied only with the written permission of MicroControl GmbH &      //
-// Co. KG or in accordance with the terms and conditions stipulated in        //
-// the agreement/contract under which the program(s) have been supplied.      //
 //----------------------------------------------------------------------------//
+// Redistribution and use in source and binary forms, with or without         //
+// modification, are permitted provided that the following conditions         //
+// are met:                                                                   //
+// 1. Redistributions of source code must retain the above copyright          //
+//    notice, this list of conditions, the following disclaimer and           //
+//    the referenced file 'LICENSE'.                                          //
+// 2. Redistributions in binary form must reproduce the above copyright       //
+//    notice, this list of conditions and the following disclaimer in the     //
+//    documentation and/or other materials provided with the distribution.    //
+// 3. Neither the name of MicroControl nor the names of its contributors      //
+//    may be used to endorse or promote products derived from this software   //
+//    without specific prior written permission.                              //
 //                                                                            //
-// Date        History                                                        //
-// ----------  -------------------------------------------------------------- //
-// 20.05.2015  Initial version                                                //
+// Provided that this notice is retained in full, this software may be        //
+// distributed under the terms of the GNU Lesser General Public License       //
+// ("LGPL") version 3 as distributed in the 'LICENSE' file.                   //
 //                                                                            //
 //============================================================================//
 
-
-//------------------------------------------------------------------------------
-// SVN  $Date: 2014-09-15 18:50:11 +0200 (Mo, 15. Sep 2014) $
-// SVN  $Rev: 6156 $ --- $Author: koppe $
-//------------------------------------------------------------------------------
 
 
 /*----------------------------------------------------------------------------*\
@@ -55,7 +52,7 @@
 // QCanFrameApi()                                                             //
 // constructor                                                                //
 //----------------------------------------------------------------------------//
-QCanFrameApi::QCanFrameApi() : CpFrameApi()
+QCanFrameApi::QCanFrameApi() : QCanData(eTYPE_API)
 {
 
 }
@@ -70,30 +67,181 @@ QCanFrameApi::~QCanFrameApi()
 
 }
 
+
+
+//----------------------------------------------------------------------------//
+// bitrate()                                                                  //
+// get bit-rate value (byte 0 .. 3)                                           //
+//----------------------------------------------------------------------------//
+bool QCanFrameApi::bitrate(int32_t & slNomBitRateV, int32_t & slDatBitRateV)
+{
+   bool  btResultT = false;
+
+   if(ulMsgMarkerP == QCanFrameApi::eAPI_FUNC_BITRATE)
+   {
+      slNomBitRateV = dataUInt32(0);
+      slDatBitRateV = dataUInt32(4);
+      btResultT = true;
+   }
+
+   return(btResultT);
+}
+
+int32_t QCanFrameApi::bitrateData(void)
+{
+   return (dataUInt32(4));
+}
+
+int32_t QCanFrameApi::bitrateNominal(void)
+{
+   return (dataUInt32(0));
+}
+
+//----------------------------------------------------------------------------//
+// function()                                                                 //
+// determine the function code                                                //
+//----------------------------------------------------------------------------//
+QCanFrameApi::ApiFunc_e QCanFrameApi::function(void)
+{
+   return((QCanFrameApi::ApiFunc_e) ulMsgMarkerP);
+
+}
+
+
+//----------------------------------------------------------------------------//
+// mode()                                                                     //
+// get operation mode                                                         //
+//----------------------------------------------------------------------------//
+CAN_Mode_e QCanFrameApi::mode(void)
+{
+   return((CAN_Mode_e ) aubByteP[0]);
+}
+
+bool QCanFrameApi::name(QString & clNameR)
+{
+   bool     btResultT = false;
+   uint8_t  ubPosT;
+
+   if(ulMsgMarkerP == QCanFrameApi::eAPI_FUNC_NAME)
+   {
+      clNameR.clear();
+      for (ubPosT = 0; ubPosT < ubMsgDlcP; ubPosT++)
+      {
+         clNameR.append(aubByteP[ubPosT]);
+      }
+      btResultT = true;
+   }
+
+   return (btResultT);
+}
+
+
+//----------------------------------------------------------------------------//
+// setBitrate()                                                               //
+// Byte 0 .. 3: slBitrateV, Byte 4 .. 7: slBrsClockV                          //
+//----------------------------------------------------------------------------//
+void QCanFrameApi::setBitrate(int32_t slBitrateV, int32_t slBrsClockV)
+{
+   ulMsgMarkerP = QCanFrameApi::eAPI_FUNC_BITRATE;
+   setDataUInt32(0, slBitrateV);
+   setDataUInt32(4, slBrsClockV);
+}
+
+
+//----------------------------------------------------------------------------//
+// setMode()                                                                  //
+// set operation mode                                                         //
+//----------------------------------------------------------------------------//
+void QCanFrameApi::setMode(CAN_Mode_e teModeV)
+{
+   ulMsgMarkerP = QCanFrameApi::eAPI_FUNC_CAN_MODE;
+   aubByteP[0] = (uint8_t) teModeV;
+}
+
+
+void QCanFrameApi::setDriverInit()
+{
+   ulMsgMarkerP = QCanFrameApi::eAPI_FUNC_DRIVER_INIT;
+
+}
+void QCanFrameApi::setDriverRelease()
+{
+   ulMsgMarkerP = QCanFrameApi::eAPI_FUNC_DRIVER_RELEASE;
+
+}
+
+void QCanFrameApi::setName(QString clNameV)
+{
+   int32_t  slSizeT;
+   int32_t  slPosT;
+
+   ulMsgMarkerP = QCanFrameApi::eAPI_FUNC_NAME;
+
+   //----------------------------------------------------------------
+   // the maximum number of characters is limited to 64
+   //
+   slSizeT = clNameV.size();
+   if(slSizeT > QCAN_MSG_DATA_MAX)
+   {
+      slSizeT = QCAN_MSG_DATA_MAX;
+   }
+
+   //----------------------------------------------------------------
+   // copy size in DLC field
+   //
+   ubMsgDlcP = (uint8_t) slSizeT;
+
+   //----------------------------------------------------------------
+   // copy string to payload
+   //
+   for (slPosT = 0; slPosT < slSizeT; slPosT++)
+   {
+      aubByteP[slPosT] = clNameV.at(slPosT).toLatin1();
+   }
+}
+
+
 //----------------------------------------------------------------------------//
 // fromByteArray()                                                            //
 //                                                                            //
 //----------------------------------------------------------------------------//
 bool QCanFrameApi::fromByteArray(const QByteArray & clByteArrayR)
 {
-   return(QCanFrame::fromByteArray(clByteArrayR));
+   bool  btResultT = false;
+   
+   //----------------------------------------------------------------
+   // an API frame must have the value 0x40 in the first byte,
+   // refer to QCanData class implementation
+   //
+   if ((clByteArrayR.at(0) & 0x40) > 0)
+   {
+      btResultT = (QCanData::fromByteArray(clByteArrayR)); 
+   }
+   
+   return (btResultT);
 }
 
+
+//----------------------------------------------------------------------------//
+// toByteArray()                                                              //
+//                                                                            //
+//----------------------------------------------------------------------------//
 QByteArray QCanFrameApi::toByteArray() const
 {
-   return QCanFrame::toByteArray();
+   return QCanData::toByteArray();
 }
+
 
 //----------------------------------------------------------------------------//
 // toString()                                                                 //
-// print CAN frame                                                            //
+// print API frame                                                            //
 //----------------------------------------------------------------------------//
 QString QCanFrameApi::toString(const bool & btShowTimeR)
 {
    //----------------------------------------------------------------
    // setup a string object
    //
-   QString clStringT; //(QCAN_FRAME_STRING_SIZE, '\0');
+   QString clStringT; 
    
    if(btShowTimeR == true)
    {
@@ -103,9 +251,22 @@ QString QCanFrameApi::toString(const bool & btShowTimeR)
    
 
    //----------------------------------------------------------------
-   // print frame format
+   // print information
    //
+   switch(function())
+   {
+      case eAPI_FUNC_BITRATE:
+         clStringT = "Bit-rate:";
+         break;
 
+      case eAPI_FUNC_NAME:
+         name(clStringT);
+         break;
+         
+      default:
+         
+         break;
+   }
    
    return(clStringT);
 }
