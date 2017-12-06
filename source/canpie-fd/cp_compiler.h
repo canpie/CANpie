6242d860e807b2b4c263e64418c69e03580d9004
Mathias Giacomuzzi <ecam@gmx.at>
2017-12-06 16:48:20 +0100

    rename compiler.h to cp_compiler.h

--- source/canpie-fd/cp_compiler.h
+++ source/canpie-fd/cp_compiler.h
@@ -1010,17 +1010,15 @@ typedef  long long            int64_t;
 
 typedef  short                Status_tv;
 
-#include "li_types.h"
-
-//typedef  unsigned char        bool_t;
-//typedef  unsigned char        uint8_t;
-//typedef  signed char          int8_t;
-//typedef  unsigned short       uint16_t;
-//typedef  short                int16_t;
-//typedef  unsigned long        uint32_t;
-//typedef  long                 int32_t;
-//typedef  unsigned long long   uint64_t;
-//typedef  long long            int64_t;
+typedef  unsigned char        bool_t;
+typedef  unsigned char        uint8_t;
+typedef  signed char          int8_t;
+typedef  unsigned short       uint16_t;
+typedef  short                int16_t;
+typedef  unsigned long        uint32_t;
+typedef  long                 int32_t;
+typedef  unsigned long long   uint64_t;
+typedef  long long            int64_t;
 
 #endif
 // End of definition:  __DCC__
