1f1e429c92da423730927f8752f1248dc929d5a6
Uwe Koppe <koppe@microcontrol.net>
2018-04-13 12:51:34 -0500

    Update the API
    New methods configureDevice(), readyRead() and version() added. The
    methods framesReceived() and framesWritten() were removed due to that
    change.

--- source/qcan/qcan_interface.hpp
+++ source/qcan/qcan_interface.hpp
@@ -1,51 +1,41 @@
-//============================================================================//
-// File:          qcan_interface.hpp                                          //
-// Description:   Virtual CAN interface class                                 //
-//                                                                            //
-// Copyright 2017 MicroControl GmbH & Co. KG                                  //
-// 53844 Troisdorf - Germany                                                  //
-// www.microcontrol.net                                                       //
-//                                                                            //
-//----------------------------------------------------------------------------//
-// Redistribution and use in source and binary forms, with or without         //
-// modification, are permitted provided that the following conditions         //
-// are met:                                                                   //
-// 1. Redistributions of source code must retain the above copyright          //
-//    notice, this list of conditions, the following disclaimer and           //
-//    the referenced file 'LICENSE'.                                          //
-// 2. Redistributions in binary form must reproduce the above copyright       //
-//    notice, this list of conditions and the following disclaimer in the     //
-//    documentation and/or other materials provided with the distribution.    //
-// 3. Neither the name of MicroControl nor the names of its contributors      //
-//    may be used to endorse or promote products derived from this software   //
-//    without specific prior written permission.                              //
-//                                                                            //
-// Licensed under the Apache License, Version 2.0 (the "License");            //
-// you may not use this file except in compliance with the License.           //
-// You may obtain a copy of the License at                                    //
-//                                                                            //
-//    http://www.apache.org/licenses/LICENSE-2.0                              //
-//                                                                            //
-// Unless required by applicable law or agreed to in writing, software        //
-// distributed under the License is distributed on an "AS IS" BASIS,          //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
-// See the License for the specific language governing permissions and        //
-// limitations under the License.                                             //
-//============================================================================//
-
-
-
-
-//-----------------------------------------------------------------------------
-/*!
-** \file    qcan_interface.hpp
-** \brief   CAN interface class
-**
-*/
+//====================================================================================================================//
+// File:          qcan_interface.hpp                                                                                  //
+// Description:   QCAN classes - CAN interface class                                                                  //
+//                                                                                                                    //
+// Copyright (C) MicroControl GmbH & Co. KG                                                                           //
+// 53844 Troisdorf - Germany                                                                                          //
+// www.microcontrol.net                                                                                               //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the   //
+// following conditions are met:                                                                                      //
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions, the following   //
+//    disclaimer and the referenced file 'LICENSE'.                                                                   //
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       //
+//    following disclaimer in the documentation and/or other materials provided with the distribution.                //
+// 3. Neither the name of MicroControl nor the names of its contributors may be used to endorse or promote products   //
+//    derived from this software without specific prior written permission.                                           //
+//                                                                                                                    //
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     //
+// with the License. You may obtain a copy of the License at                                                          //
+//                                                                                                                    //
+//    http://www.apache.org/licenses/LICENSE-2.0                                                                      //
+//                                                                                                                    //
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   //
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  //
+// the specific language governing permissions and limitations under the License.                                     //
+//                                                                                                                    //
+//====================================================================================================================//
 
 
 #ifndef QCAN_INTERFACE_HPP_
 #define QCAN_INTERFACE_HPP_
+
+/*--------------------------------------------------------------------------------------------------------------------*\
+** Include files                                                                                                      **
+**                                                                                                                    **
+\*--------------------------------------------------------------------------------------------------------------------*/
+
 
 #include <stdint.h>
 #include "qcan_defs.hpp"
@@ -53,18 +43,16 @@
 #include "qcan_frame_api.hpp"
 #include "qcan_frame_error.hpp"
 
-
 using namespace QCan;
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
 /*!
 ** \class   QCanInterface
 ** \brief   CAN interface
 ** 
-** The QCanInterface class describes one physical CAN interface. The CAN
-** interface is typically implemented inside a CAN plug-in (see QCanPlugin).
-** After connection to the CAN interface (see connect()) the use can read and
-** write CAN frames via this hardware.
+** The QCanInterface class describes one physical CAN interface. The CAN interface is typically implemented
+** inside a CAN plug-in (see QCanPlugin). After connection to the CAN interface (see connect()) the use can
+** read and write CAN frames via this hardware.
 */
 class QCanInterface : public QObject
 {
@@ -85,21 +73,24 @@ public:
    ** This enumeration describes possible error conditions.
    */
    enum InterfaceError_e {
+      eERROR_CHANNEL = -4,
+
+      /*! Access to library / plugin failed           */
+      eERROR_LIBRARY = -3,
+
+      /*! CAN interface error                         */
+      eERROR_DEVICE   = -2,
+
       /*! An unknown error occurred.                  */
       eERROR_UNKNOWN = -1,
 
       /*! No error occurred.                          */
       eERROR_NONE,
 
-      /*! Access to library / plugin failed           */
-      eERROR_LIBRARY,
 
-      eERROR_CHANNEL,
 
       /*! Bit-rate not supported                      */
       eERROR_BITRATE,
-
-      eERROR_DEVICE,
 
       eERROR_MODE,
 
@@ -114,7 +105,17 @@ public:
       eERROR_FIFO_TRM_FULL
    };
 
+   //---------------------------------------------------------------------------------------------------
+   /*!
+   ** \return  true if the interface settings have been changed
+   **
+   ** This method is used to initiate a device specific configuration which is out of scope of this
+   ** interface class. A return value of \a true means the interface settings have been changed.
+   */
+   inline virtual bool        configureDevice(void) { return (false); };
 
+
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \return     Status code defined by InterfaceError_e
    ** \see        disconnect()
@@ -123,6 +124,8 @@ public:
    */
    virtual InterfaceError_e connect(void) = 0;
 
+
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \return     \c true if connection is possible
    ** \see connected()
@@ -131,6 +134,8 @@ public:
    */
    virtual bool connected(void) = 0;
 
+
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \return     Status code defined by InterfaceError_e
    ** \see        connect()
@@ -139,7 +144,7 @@ public:
    */
    virtual InterfaceError_e disconnect(void) = 0;
 
-
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \return     Icon
    **
@@ -148,6 +153,7 @@ public:
    virtual QIcon icon(void) = 0;
 
 
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \return     Name
    **
@@ -155,42 +161,45 @@ public:
    */
    virtual QString name(void) = 0;
 
+
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \param[out] clDataR  Message stream
    ** \return     Status code defined by InterfaceError_e
    ** \see        read()
    **
-   ** The functions reads a CAN message (data or error frame) from the 
-   ** CAN interface. The data is copied to the QByteArray \a clDataR.
-   ** If no message is available, the function will return the value
-   ** eERROR_FIFO_RCV_EMPTY. On success the function returns eERROR_NONE.
+   ** The functions reads a CAN message (data or error frame) from the CAN interface. The data is
+   ** copied to the QByteArray \a clDataR. If no message is available, the function will return the
+   ** value eERROR_FIFO_RCV_EMPTY. On success the function returns eERROR_NONE.
    **
    */
    virtual InterfaceError_e   read( QByteArray &clDataR) = 0;
+
    
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \return     Status code defined by InterfaceError_e
    ** \see        reset()
    **
-   ** The functions resets a CAN a physical CAN interface.
-   ** All statistic informations will be reset also.
+   ** The functions resets a CAN a physical CAN interface. All statistic information will be reset also.
    ** On success the function returns eERROR_NONE.
    */
    virtual InterfaceError_e   reset(void) = 0;
 
+
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \param[in]  slNomBitRateV  Nominal Bit-rate value
    ** \param[in]  slDatBitRateV  Data Bit-rate value
    ** \return     Status code defined by InterfaceError_e
    **
-   **	This function sets the bit-rate of the CAN interface. If the physical
-   **	CAN interface does not support CAN FD, the parameter \c slDatBitRateV
-   **	is not evaluated.
+   **	This function sets the bit-rate of the CAN interface. If the physical CAN interface does not
+   **	support CAN FD, the parameter \c slDatBitRateV is not evaluated.
    */
-   virtual InterfaceError_e   setBitrate(int32_t slNomBitRateV,
-                                         int32_t slDatBitRateV = eCAN_BITRATE_NONE) = 0;
+   virtual InterfaceError_e   setBitrate(int32_t slNomBitRateV, int32_t slDatBitRateV = eCAN_BITRATE_NONE) = 0;
 
 
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \param[in]  teModeV CAN mode 
    ** \return     Status code defined by InterfaceError_e
@@ -212,25 +221,60 @@ public:
    virtual uint32_t           supportedFeatures(void) = 0;
 	
 
+   //---------------------------------------------------------------------------------------------------
+   /*!
+   ** \return     Version of the CAN interface
+   **
+   ** The function returns version information of the physical CAN interface.
+   **
+   */
+   virtual QString            version(void) { return QString("No version information available"); };
+
+
+   //---------------------------------------------------------------------------------------------------
    /*!
    ** \param[in]  clFrameR CAN data frame
    ** \return     Status code defined by InterfaceError_e
    ** \see        read()
    **
-   ** The functions writes a CAN message (data frame) to the 
-   ** CAN interface. The CAN frame data is defined by the parameter 
-   ** \a clFrameR. On success the function returns eERROR_NONE.
+   ** The functions writes a CAN message (data frame) to the CAN interface. The CAN frame data is
+   ** defined by the parameter \a clFrameR. On success the function returns eERROR_NONE.
    **
    */
-   virtual InterfaceError_e   	write(const QCanFrame &clFrameR) = 0;
-
+   virtual InterfaceError_e   write(const QCanFrame &clFrameR) = 0;
 
 
 Q_SIGNALS:
-    void errorOccurred(int32_t slCanBusErrorV);     //  QCanBusDevice::CanBusError
-    void framesReceived(uint32_t ulFramesCountV);
-    void framesWritten(uint32_t ulFramesCountV);
-    void stateChanged(int32_t slCanBusDevStatusV);
+
+   //---------------------------------------------------------------------------------------------------
+   /*!
+   ** \param[in]  clMessageV - Logging message
+   ** \param[in]  teLogLevel - Logging level
+   **
+   ** This signal is emitted by the CAN interface to inform the application about status changes or
+   ** error conditions.
+   **
+   */
+   void addLogMessage(const QString & clMessageR, const LogLevel_e & teLogLevelR = eLOG_LEVEL_NOTICE);
+
+
+   //---------------------------------------------------------------------------------------------------
+   /*!
+   ** This signal is emitted once every time new data is available for reading from the device's current
+   ** read channel. It will only be emitted again once new data is available.
+   */
+   void readyRead(void);
+
+
+   //---------------------------------------------------------------------------------------------------
+   /*!
+   ** \param[in]  teCanStateR - CAN state
+   **
+   ** This signal is emitted by the CAN interface to inform the application about state changes of
+   ** the CAN controller.
+   **
+   */
+   void stateChanged(const CAN_State_e & teCanStateR);
 
 private:
 
