469960fcc96ba99579d1ba26dfae2fbbd17bbfac
Uwe Koppe <koppe@microcontrol.net>
2018-04-13 13:03:11 -0500

    Add initial support for server settings

--- source/qcan/qcan_server.cpp
+++ source/qcan/qcan_server.cpp
@@ -1,70 +1,63 @@
-//============================================================================//
-// File:          qcan_network.cpp                                            //
-// Description:   QCAN classes - CAN network                                  //
-//                                                                            //
-// Copyright 2017 MicroControl GmbH & Co. KG                                  //
-// 53844 Troisdorf - Germany                                                  //
-// www.microcontrol.net                                                       //
-//                                                                            //
-//----------------------------------------------------------------------------//
-// Redistribution and use in source and binary forms, with or without         //
-// modification, are permitted provided that the following conditions         //
-// are met:                                                                   //
-// 1. Redistributions of source code must retain the above copyright          //
-//    notice, this list of conditions, the following disclaimer and           //
-//    the referenced file 'LICENSE'.                                          //
-// 2. Redistributions in binary form must reproduce the above copyright       //
-//    notice, this list of conditions and the following disclaimer in the     //
-//    documentation and/or other materials provided with the distribution.    //
-// 3. Neither the name of MicroControl nor the names of its contributors      //
-//    may be used to endorse or promote products derived from this software   //
-//    without specific prior written permission.                              //
-//                                                                            //
-// Licensed under the Apache License, Version 2.0 (the "License");            //
-// you may not use this file except in compliance with the License.           //
-// You may obtain a copy of the License at                                    //
-//                                                                            //
-//    http://www.apache.org/licenses/LICENSE-2.0                              //
-//                                                                            //
-// Unless required by applicable law or agreed to in writing, software        //
-// distributed under the License is distributed on an "AS IS" BASIS,          //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
-// See the License for the specific language governing permissions and        //
-// limitations under the License.                                             //
-//============================================================================//
+//====================================================================================================================//
+// File:          qcan_server.cpp                                                                                     //
+// Description:   QCAN classes - CAN server                                                                           //
+//                                                                                                                    //
+// Copyright (C) MicroControl GmbH & Co. KG                                                                           //
+// 53844 Troisdorf - Germany                                                                                          //
+// www.microcontrol.net                                                                                               //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the   //
+// following conditions are met:                                                                                      //
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions, the following   //
+//    disclaimer and the referenced file 'LICENSE'.                                                                   //
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       //
+//    following disclaimer in the documentation and/or other materials provided with the distribution.                //
+// 3. Neither the name of MicroControl nor the names of its contributors may be used to endorse or promote products   //
+//    derived from this software without specific prior written permission.                                           //
+//                                                                                                                    //
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     //
+// with the License. You may obtain a copy of the License at                                                          //
+//                                                                                                                    //
+//    http://www.apache.org/licenses/LICENSE-2.0                                                                      //
+//                                                                                                                    //
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   //
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  //
+// the specific language governing permissions and limitations under the License.                                     //
+//                                                                                                                    //
+//====================================================================================================================//
 
 
-/*----------------------------------------------------------------------------*\
-** Include files                                                              **
-**                                                                            **
-\*----------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------*\
+** Include files                                                                                                      **
+**                                                                                                                    **
+\*--------------------------------------------------------------------------------------------------------------------*/
 
-#include <QDebug>
+#include <QtCore/QDebug>
 
 #include "qcan_server.hpp"
+#include "qcan_server_memory.hpp"
+
+
+/*--------------------------------------------------------------------------------------------------------------------*\
+** Definitions                                                                                                        **
+**                                                                                                                    **
+\*--------------------------------------------------------------------------------------------------------------------*/
 
 
 
-/*----------------------------------------------------------------------------*\
-** Definitions                                                                **
-**                                                                            **
-\*----------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------------------------------------------------*\
+** Class methods QCanServer                                                                                           **
+**                                                                                                                    **
+\*--------------------------------------------------------------------------------------------------------------------*/
 
 
-
-/*----------------------------------------------------------------------------*\
-** Class methods                                                              **
-**                                                                            **
-\*----------------------------------------------------------------------------*/
-
-
-//----------------------------------------------------------------------------//
-// QCanServer()                                                               //
-// constructor                                                                //
-//----------------------------------------------------------------------------//
-QCanServer::QCanServer( QObject * pclParentV,
-                        uint16_t  uwPortStartV,
-                        uint8_t   ubNetworkNumV)
+//--------------------------------------------------------------------------------------------------------------------//
+// QCanServer()                                                                                                       //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
+QCanServer::QCanServer( QObject * pclParentV, uint16_t  uwPortStartV, uint8_t   ubNetworkNumV)
 {
    QCanNetwork *  pclCanNetT;
 
@@ -72,8 +65,6 @@ QCanServer::QCanServer( QObject * pclParentV,
    // set the parent
    //
    this->setParent(pclParentV);
-
-   ulDispatchTimeP = 10;
 
    //----------------------------------------------------------------
    // create CAN networks
@@ -88,33 +79,91 @@ QCanServer::QCanServer( QObject * pclParentV,
 
    }
 
+   initSettings();
 }
 
 
-//----------------------------------------------------------------------------//
-// QCanServer()                                                               //
-// destructor                                                                 //
-//----------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------------------//
+// ~QCanServer()                                                                                                      //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
 QCanServer::~QCanServer()
 {
+   releaseSettings();
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------//
+// QCanServer::enableBitrateChange()                                                                                  //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
+void QCanServer::enableBitrateChange(bool btEnabledV)
+{
+   QCanNetwork *  pclNetworkT;
+
+   for(uint8_t ubNetCntT = 0; ubNetCntT < maximumNetwork(); ubNetCntT++)
+   {
+      pclNetworkT = network(ubNetCntT);
+
+      //-------------------------------------------------------------
+      // assign new time value
+      //
+      pclNetworkT->setBitrateFrameEnabled(btEnabledV);
+   }
 
 }
 
+
+//--------------------------------------------------------------------------------------------------------------------//
+// QCanServer::initSettings()                                                                                         //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
+void QCanServer::initSettings(void)
+{
+   pclSettingsP = new QSharedMemory(QString(QCAN_MEMORY_KEY));
+   btMemoryAttachedP = pclSettingsP->create(sizeof(ServerSettings_ts), QSharedMemory::ReadWrite);
+
+   if (btMemoryAttachedP)
+   {
+      pclSettingsP->lock();
+      memset(pclSettingsP->data(), 0, sizeof(ServerSettings_ts));
+      pclSettingsP->unlock();
+   }
+   else
+   {
+      btMemoryAttachedP = pclSettingsP->attach();
+   }
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------//
+// QCanServer::maximumNetwork()                                                                                       //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
 uint8_t QCanServer::maximumNetwork(void) const
 {
    return(pclListNetsP->count());
 }
 
 
-//----------------------------------------------------------------------------//
-// network()                                                                  //
-//                                                                            //
-//----------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------------------//
+// QCanServer::network()                                                                                              //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
 QCanNetwork * QCanServer::network(uint8_t ubNetworkIdxV)
 {
    return(pclListNetsP->at(ubNetworkIdxV));
 }
 
+
+//--------------------------------------------------------------------------------------------------------------------//
+// QCanServer::releaseSettings()                                                                                      //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
+void QCanServer::releaseSettings(void)
+{
+   pclSettingsP->detach();
+}
 
 //----------------------------------------------------------------------------//
 // setHostAddress()                                                           //
@@ -127,6 +176,8 @@ void QCanServer::setServerAddress(QHostAddress clHostAddressV)
    if (!clHostAddressV.isEqual(clServerAddressP))
    {
       clServerAddressP = clHostAddressV;
+
+      qDebug() << "QCanServer::setServerAddress()" << clHostAddressV;
 
       for(uint8_t ubNetCntT = 0; ubNetCntT < maximumNetwork(); ubNetCntT++)
       {
@@ -156,24 +207,3 @@ void QCanServer::setServerAddress(QHostAddress clHostAddressV)
 }
 
 
-//----------------------------------------------------------------------------//
-// setDispatcherTime()                                                        //
-//                                                                            //
-//----------------------------------------------------------------------------//
-void QCanServer::setDispatcherTime(uint32_t ulTimeV)
-{
-   QCanNetwork *  pclNetworkT;
-
-   ulDispatchTimeP = ulTimeV;
-
-   for(uint8_t ubNetCntT = 0; ubNetCntT < maximumNetwork(); ubNetCntT++)
-   {
-      pclNetworkT = network(ubNetCntT);
-
-      //-------------------------------------------------------------
-      // assign new time value
-      //
-      pclNetworkT->setDispatcherTime(ulTimeV);
-
-   }
-}
--- source/qcan/qcan_server.hpp
+++ source/qcan/qcan_server.hpp
@@ -1,48 +1,43 @@
-//============================================================================//
-// File:          qcan_network.hpp                                            //
-// Description:   QCAN classes - CAN network                                  //
-//                                                                            //
-// Copyright 2017 MicroControl GmbH & Co. KG                                  //
-// 53844 Troisdorf - Germany                                                  //
-// www.microcontrol.net                                                       //
-//                                                                            //
-//----------------------------------------------------------------------------//
-// Redistribution and use in source and binary forms, with or without         //
-// modification, are permitted provided that the following conditions         //
-// are met:                                                                   //
-// 1. Redistributions of source code must retain the above copyright          //
-//    notice, this list of conditions, the following disclaimer and           //
-//    the referenced file 'LICENSE'.                                          //
-// 2. Redistributions in binary form must reproduce the above copyright       //
-//    notice, this list of conditions and the following disclaimer in the     //
-//    documentation and/or other materials provided with the distribution.    //
-// 3. Neither the name of MicroControl nor the names of its contributors      //
-//    may be used to endorse or promote products derived from this software   //
-//    without specific prior written permission.                              //
-//                                                                            //
-// Licensed under the Apache License, Version 2.0 (the "License");            //
-// you may not use this file except in compliance with the License.           //
-// You may obtain a copy of the License at                                    //
-//                                                                            //
-//    http://www.apache.org/licenses/LICENSE-2.0                              //
-//                                                                            //
-// Unless required by applicable law or agreed to in writing, software        //
-// distributed under the License is distributed on an "AS IS" BASIS,          //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
-// See the License for the specific language governing permissions and        //
-// limitations under the License.                                             //
-//============================================================================//
+//====================================================================================================================//
+// File:          qcan_server.hpp                                                                                     //
+// Description:   QCAN classes - CAN server                                                                           //
+//                                                                                                                    //
+// Copyright (C) MicroControl GmbH & Co. KG                                                                           //
+// 53844 Troisdorf - Germany                                                                                          //
+// www.microcontrol.net                                                                                               //
+//                                                                                                                    //
+//--------------------------------------------------------------------------------------------------------------------//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the   //
+// following conditions are met:                                                                                      //
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions, the following   //
+//    disclaimer and the referenced file 'LICENSE'.                                                                   //
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       //
+//    following disclaimer in the documentation and/or other materials provided with the distribution.                //
+// 3. Neither the name of MicroControl nor the names of its contributors may be used to endorse or promote products   //
+//    derived from this software without specific prior written permission.                                           //
+//                                                                                                                    //
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     //
+// with the License. You may obtain a copy of the License at                                                          //
+//                                                                                                                    //
+//    http://www.apache.org/licenses/LICENSE-2.0                                                                      //
+//                                                                                                                    //
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   //
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  //
+// the specific language governing permissions and limitations under the License.                                     //
+//                                                                                                                    //
+//====================================================================================================================//
 
 
 #ifndef QCAN_SERVER_HPP_
 #define QCAN_SERVER_HPP_
 
 
-/*----------------------------------------------------------------------------*\
-** Include files                                                              **
-**                                                                            **
-\*----------------------------------------------------------------------------*/
-#include <QObject>
+/*--------------------------------------------------------------------------------------------------------------------*\
+** Include files                                                                                                      **
+**                                                                                                                    **
+\*--------------------------------------------------------------------------------------------------------------------*/
+#include <QtCore/QObject>
+#include <QtCore/QSharedMemory>
 
 #include "qcan_network.hpp"
 
@@ -53,38 +48,54 @@
 ** \brief CAN server
 **
 ** This class represents a CAN server, which incorporates up to
-** QCAN_NETWORK_MAX number of CAN networks (QCanNetwork).
+** #QCAN_NETWORK_MAX number of CAN networks (QCanNetwork).
+** <p>
+** The QCanServerDialog class provides an example application.
+**
 **
 */
 class QCanServer : public QObject
 {
-    Q_OBJECT
+   Q_OBJECT
 
 public:
-    QCanServer( QObject * pclParentV = Q_NULLPTR,
-                uint16_t  uwPortStartV = QCAN_TCP_DEFAULT_PORT,
-                uint8_t   ubNetworkNumV = QCAN_NETWORK_MAX);
+   /*!
+   ** Construct a QCanServer object
+   */
+   QCanServer( QObject * pclParentV = Q_NULLPTR,
+               uint16_t  uwPortStartV = QCAN_TCP_DEFAULT_PORT,
+               uint8_t   ubNetworkNumV = QCAN_NETWORK_MAX);
 
-    ~QCanServer();
+   ~QCanServer();
 
+   void           enableBitrateChange(bool btEnabledV = true);
 
-    uint32_t      dispatcherTime(void)    { return (ulDispatchTimeP);   };
+   bool           isActive(void)          { return (btMemoryAttachedP); };
 
-    QCanNetwork * network(uint8_t ubNetworkIdxV);
+   /*!
+   ** \return Pointer to QCanNetwork
+   ** \param[in]  ubNetworkIndexV - Network index
+   **
+   ** The function returns a pointer to a QCanNetwork class stored at index
+   ** \a ubNetworkIndexV. The first networl ist stored at index value 0.
+   */
+   QCanNetwork * network(uint8_t ubNetworkIndexV);
 
-    uint8_t       maximumNetwork(void) const;
+   uint8_t       maximumNetwork(void) const;
 
-    void          setDispatcherTime(uint32_t ulTimeV);
+   QHostAddress  serverAddress(void)     { return (clServerAddressP);  };
 
-    QHostAddress  serverAddress(void)     { return (clServerAddressP);  };
-
-    void          setServerAddress(QHostAddress clHostAddressV);
+   void          setServerAddress(QHostAddress clHostAddressV);
 
 private:
 
-    QVector<QCanNetwork *> *  pclListNetsP;
-    QHostAddress              clServerAddressP;
-    uint32_t                  ulDispatchTimeP;
+   void     initSettings(void);
+   void     releaseSettings(void);
+
+   QVector<QCanNetwork *> *   pclListNetsP;
+   QHostAddress               clServerAddressP;
+   QSharedMemory *            pclSettingsP;
+   bool                       btMemoryAttachedP;
 };
 
 #endif // QCAN_SERVER_HPP_
